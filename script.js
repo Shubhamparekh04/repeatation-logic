67cf81375cdfb60c05f78f140bdecae51009fc91
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-06-28 12:20:43 +0530

    Repetition logic

---
+++ script.js
@@ -0,0 +1,104 @@
+// question 1
+
+// const userAge = prompt("Enter your Age please :");
+
+// if (userAge > 18) {
+//     console.log("Welcome, You are eligible!");
+// } else {
+//     console.log("Sorry, You are not eligible");
+// }
+
+
+// question 2
+
+// const currentTemp = prompt("Enter Temperature :");
+
+
+// if (currentTemp > 30){
+//     console.log("It's hot outside !");
+// } else {
+//     if(currentTemp > 20 && currentTemp < 30){
+//         console.log("It's Pleasant day!");
+//     }else {
+//         console.log("It's cold outside !");
+//     }
+// }
+
+
+
+
+
+// question 3
+
+// const score = 59;
+
+
+// if (score > 90) {
+//     console.log("Grade A");
+// } else {
+
+//     if (score > 80 && score < 89) {
+//         console.log("Grade B");
+//     } else {
+//         if (score > 70 && score < 79) {
+//             console.log("Grade C");
+//         } else {
+//             if (score > 60 && score < 69) {
+//                 console.log("Grade D");
+//             } else {
+//                 console.log("Grade F");
+//             }
+//         }
+//     }
+// }
+
+
+// question 4
+
+
+// const userAge = prompt("Enter your Age please :");
+
+// if (userAge <= 12) {
+//     console.log("Child ticket: $10");
+// } else {
+//     if (userAge >= 13 && userAge<=64){
+//         console.log("Adult ticket: $20");
+//     } else {
+//         console.log("Senior citizen ticket: $15");
+//     }
+// }
+
+
+// question 5
+
+// const dayOfWeek = prompt("Enter day number:");
+
+// if (dayOfWeek == 1) {
+//     console.log("Monday");
+// } else {
+//     if (dayOfWeek == 2) {
+//         console.log("Tuesday");
+//     } else {
+//         if (dayOfWeek == 3) {
+//             console.log("Wednesday");
+//         } else {
+//             if (dayOfWeek == 4) {
+//                 console.log("Thursday");
+//             } else {
+//                 if (dayOfWeek == 5) {
+//                     console.log("Friday");
+//                 } else {
+//                     if (dayOfWeek == 6) {
+//                         console.log("Saturday");
+//                     } else {
+//                         if (dayOfWeek == 7) {
+//                             console.log("Sunday");
+//                         } else {
+//                             console.log("Invalid day number");
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
